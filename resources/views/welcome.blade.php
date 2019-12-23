693a71009b716f942a811a77afd0ae6fbd0e3313
Stakvino <stakvino@gmail.com>
2019-12-23 12:01:40 +0100

    Heroku Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Heroku
+                    Laravel Heroku With Github
                 </div>
 
                 <div class="links">
